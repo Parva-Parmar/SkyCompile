a1ef5049b270509640d28c6912869aaf75a77889
Parva Parmar <parva004cp@gmail.com>
2026-02-15 12:48:53 +0530

    wsl-phase-1

---
+++ backend-spring/src/main/java/com/skycompile/backend_spring/repositories/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.skycompile.backend_spring.repositories;
+
+import com.skycompile.backend_spring.entities.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+}
